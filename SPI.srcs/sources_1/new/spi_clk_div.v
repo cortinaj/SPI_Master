4241e306f1ad4351e3203fa05886ccb9616c5b9d
cortinaj <johnpaul.cortina.716@my.csun.edu>
2026-06-15 23:04:21 -0700

    Designed clk divider for SPI Master. Will implement testbench tomorrow.

---
+++ SPI.srcs/sources_1/new/spi_clk_div.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/15/2026 10:15:41 PM
+// Design Name: 
+// Module Name: spi_clk_div
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module spi_clk_div(
+
+    );
+endmodule
