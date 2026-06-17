0b544970110c46d9018abf175be895a46e82b90f
cortinaj <johnpaul.cortina.716@my.csun.edu>
2026-06-16 18:41:41 -0700

    Implemented clock divider logic.

--- SPI.srcs/sources_1/new/spi_clk_div.v
+++ SPI.srcs/sources_1/new/spi_clk_div.v
@@ -21,6 +21,37 @@
 
 
 module spi_clk_div(
-
+    input sys_clk,
+    input sys_rst_n,
+    input [7:0] clk_div,
+    input en,
+    output reg fall_en,
+    output reg rise_en
     );
+    
+    reg [7:0] counter;
+    
+    always@(posedge sys_clk) begin
+        if(!sys_rst_n) begin
+            counter <= 8'b0;
+            fall_en <= 1'b0;
+            rise_en <= 1'b0;
+        end else if (!en) begin
+            counter <= 8'b0;
+            fall_en <= 1'b0;
+            rise_en <= 1'b0;
+        end else begin
+            fall_en <= 1'b0;
+            rise_en <= 1'b0;
+            if(counter == clk_div) begin
+                fall_en <= 1'b1;
+                counter <= 8'b0;
+            end else begin
+                if(counter == clk_div >> 1) begin
+                    rise_en <= 1'b1;
+                end
+                counter <= counter + 1;
+           end
+       end
+   end
 endmodule
